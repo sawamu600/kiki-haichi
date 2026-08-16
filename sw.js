204f80e79153184feb6d6649ae4cbb116e5c68ff
sawamu600 <n6k9s5cygf@privaterelay.appleid.com>
2026-08-16 12:25:23 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='kiki-haichi-v4';
+const ASSETS=['./','./index.html','./manifest.json','./icon-180.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
